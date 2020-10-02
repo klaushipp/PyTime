b00f316d9e65de0c61c6204e571097b5434c02a8
Vincent Schmidt <superdingdong@gmx.de>
2020-10-02 22:22:58 +0200

    StandardApp rausgeschmissen und durch NPSAppManaged ersetzt.

--- main_tui.py
+++ main_tui.py
@@ -5,31 +5,26 @@ import curses
 import time
 import npyscreen
 
-def print_banner():
-    window = curses.newwin(6, 30, 0, 0)
-
-    pytime_ascii_str = (" ___     _____ _           \n"
-                        "| _ \_  |_   _(_)_ __  ___ \n"
-                        "|  _/ || || | | | '  \/ -_)\n"
-                        "|_|  \_, ||_| |_|_|_|_\___|\n"
-                        "     |__/                  \n"
-                        )
-
-    window.addstr(pytime_ascii_str)
-    window.refresh()
-    return window
-
 class MenuList(npyscreen.BoxTitle):
-    def __init__(self, *args, **keywords):
-        super(MenuList, self).__init__(*args, **keywords)
-
     def when_value_edited(self):
-        self.parent.parentApp.queue_event(npyscreen.Event("event_menu_select"))
+        self.parent.update_edit_content()
 
-class SideMenu(npyscreen.Form):
+    def when_check_value_changed(self):
+        self.parent.update_change_content()
+
+    def when_cursor_moved(self):
+        self.parent.update_cursor_content()
+
+    def when_check_cursor_moved(self):
+        self.parent.update_cursor_content()
+
+class ContentBox(npyscreen.BoxTitle):
     def create(self):
-        self.Text1 = self.add(npyscreen.TitleText, name = "Name")
-        self.Date = self.add(npyscreen.TitleDateCombo, name = "Date blabla")
+        pass
+
+    def show_something(self, content):
+        self.values = content
+        self.display()
 
 class MainForm(npyscreen.FormBaseNew):
     def afterEditing(self):
@@ -37,17 +32,25 @@ class MainForm(npyscreen.FormBaseNew):
         
     def create(self):
         y,x = self.useable_space()
+
         self.menu_list = self.add(MenuList, 
                                   name = "Menu",
                                   values = ["Time", "Timer", "Exit"],
-                                  max_width = x //4)
-        # self.button_Time = self.add(npyscreen.Menu, name = "Time")
-        # self.button_Timer = self.add(npyscreen.Button, name = "Timer")
-        # self.button_Exit = self.add(npyscreen.Button, name = "Exit")
+                                  max_width = x // 4)
 
-    def event_menu_select(self, event):
-        pass
+        self.content_window = self.add(ContentBox,
+                                       name = "Content",
+                                       relx = x // 4 + 5,
+                                       rely = 2)
+                                    
+    def update_edit_content(self):
+        self.content_window.show_something("edit")
 
+    def update_change_content(self):
+        self.content_window.show_something("change")
+
+    def update_cursor_content(self):
+        self.content_window.show_something("cursor")
 
 class TestApp(npyscreen.NPSAppManaged):
     def onStart(self):
